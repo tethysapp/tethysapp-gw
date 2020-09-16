669024be52fd84f37107317f687b73e44517781e
Rohit Khattar <rohitkh@byu.edu>
2020-09-16 13:59:18 -0600

    Fix Setup.py for tethys3

--- setup.py
+++ setup.py
@@ -6,8 +6,6 @@ from tethys_apps.app_installation import find_resource_files
 ### Apps Definition ###
 app_package = 'gw'
 release_package = 'tethysapp-' + app_package
-app_class = 'gw.app:Gw'
-app_package_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tethysapp', app_package)
 
 # -- Get Resource File -- #
 resource_files = find_resource_files('tethysapp/' + app_package + '/templates')
@@ -16,7 +14,7 @@ resource_files += find_resource_files('tethysapp/' + app_package + '/public')
 
 setup(
     name=release_package,
-    version='0.0.1',
+    version='1.0.0',
     description='This application uses spatial and temporal interpolation of well data to create groundwater level maps and time series.',
     long_description='',
     keywords='',
@@ -24,9 +22,8 @@ setup(
     author_email='stevenwevans2@gmail.com',
     url='',
     license='',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    packages=find_namespace_packages(),
     package_data={'': resource_files},
-    namespace_packages=['tethysapp', 'tethysapp.' + app_package],
     include_package_data=True,
     zip_safe=False,
     install_requires=[]
